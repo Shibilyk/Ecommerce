ff8c6564be17729eb3e2f5bde75df818eea43f44
Shibilyk <shibilyk11@gmail.com>
2024-06-01 16:22:01 +0530

    Add new files

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.render('./admin/adminLogin');
 });
 
 app.listen(PORT, () => {
