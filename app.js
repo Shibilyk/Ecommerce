357e126410aee0d5d45df535a1d950b8095086b9
Shibilyk <shibilyk11@gmail.com>
2024-06-05 23:26:41 +0530

    product file add

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const adminRouter = require('./router/admin/adminRouter');
 require('dotenv').config();
 const PORT = process.env.PORT;
+
+
+connectDB();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render('./admin/adminHome');
-});
-app.get("/login",(req,res)=>{
-  res.render('./user/userLogin')
-})
+
+
+app.use('/',adminRouter)
+
 app.listen(PORT, () => {
   console.log("server start...");
 });
+
